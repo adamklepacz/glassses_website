539f835d52151dfa95740fa447bf91dfa2f3c6b6
Adam Klepacz <adamklepacz92@gmail.com>
2018-10-25 21:54:03 +0200

    Add jqeury logic

--- app/index.js
+++ app/index.js
@@ -8,6 +8,29 @@ import 'styles/index.scss';
 // ================================
 // START YOUR APP HERE
 // ================================
-import 'jquery';
+import $ from 'jquery';
 import 'popper.js';
 import 'bootstrap';
+
+(function () {
+  'use strict';
+
+  const prevButton = $('#cta__prev');
+  const nextButton = $('#cta__next');
+  const peopleSection = $('#people');
+  const glassesSection = $('#glasses');
+
+  nextButton.on('click', function () {
+    peopleSection.fadeOut('slow').addClass('d-none').removeClass('d-flex');
+    glassesSection.fadeIn('slow').removeClass('d-none').addClass('d-flex');
+    nextButton.addClass('d-none');
+    prevButton.removeClass('d-none');
+  });
+
+  prevButton.on('click', function () {
+    glassesSection.fadeOut('slow').addClass('d-none').removeClass('d-flex');
+    peopleSection.fadeIn('slow').removeClass('d-none').addClass('d-flex');
+    nextButton.removeClass('d-none');
+    prevButton.addClass('d-none');
+  });
+}());
